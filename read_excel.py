73bdb46459811377efac9e9aede0656299b0e728
Aaron John <aaron.john@sada.com>
2023-02-01 14:19:47 +0530

    commit

--- read_excel.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel(r'/Users/aaron.john/Desktop/Composer/SPFD_Emission Factors_All Contributors.xlsx', names=['Contributor', 'Sub Category', 'Category', 'Multiplying Factor', 'Area code', 'Feature Factors', 'NULL', 'NULL'],header=1)
-df.fillna(value="NULL", inplace=True)
-print(df)
