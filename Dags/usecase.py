086b907d05d8b960be6238fc34ca393b0fc7567d
Aaron John <aaron.john@sada.com>
2023-02-06 13:37:10 +0530

    change in usecase.py

--- Dags/usecase.py
+++ Dags/usecase.py
@@ -24,17 +24,17 @@ client.setup_logging()
 with DAG("Storage_trigger_response_dag",start_date=datetime(2021, 1 ,1),
  schedule_interval=None,catchup=False) as dag:
 
-    task_model_A = ShortCircuitOperator(
-        
+    task_model_A = ShortCircuitOperator(               # this short circuit operator will skip the next task if the value returned is false which in this case will be  if the file has a wrong extension
+                                        
         task_id='fileValidation',
         python_callable=extensionCheck
         )
-    task_model_B = ShortCircuitOperator(
+    task_model_B = ShortCircuitOperator(                # this short circuit operator will skip the next task if the value returned is false which in this case will be  if the file has the wrong schema
         
         task_id='Schema_Check',
         python_callable=schemaCheck
         )
-    task_model_C = PythonOperator(
+    task_model_C = PythonOperator(                     # This python operator helps to ingest the data into the bigquery
 
             task_id="Biqquery_ingestion",
             python_callable=ingestionToBQ
